ac460b682e9bd76667bae8a053aed03615d10a4a
ruvais <ruvaispuv@gmail.com>
2026-04-04 17:46:02 +0530

    New logo

--- src/components/Header/Logo/index.tsx
+++ src/components/Header/Logo/index.tsx
@@ -6,7 +6,7 @@ const Logo: React.FC = () => {
     return (
         <Link href="/" className="flex items-center group">
             <Image
-                src="/logo/YACS pixel LOGO (1).png"
+                src="/logo/YACS pixel LOGO (2).png"
                 alt="YACS Logo"
                 width={2242}
                 height={686}
