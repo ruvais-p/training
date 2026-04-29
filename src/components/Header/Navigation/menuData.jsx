1678ae2d6df6f7962bd60dfedd37afd232199c8d
ruvais <ruvaispuv@gmail.com>
2026-04-30 00:56:17 +0530

    header updated

--- src/components/Header/Navigation/menuData.jsx
+++ src/components/Header/Navigation/menuData.jsx
@@ -1,6 +1,4 @@
 export const headerData = [
     { label: "About", href: "/#about" },
     { label: "Mentors", href: "/#mentors" },
-    { label: "Tools", href: "/#tools" },
-    { label: "FAQ", href: "/#faq" },
 ];
